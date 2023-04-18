086e821838868914959c89b5fc070aec9417753b
Ishmael M. Dukuly <99619343+IshmaeDukuly@users.noreply.github.com>
2023-04-18 22:41:41 +0000

    syntax.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const person = 'IShmael';
+
+let firstName = "Moisema";
+
+// console.log(firstName);
+
+firstName = 'james';
+console.log(firstName.length);
+console.log(person.toUpperCase());
---
+++ syntax.js
@@ -0,0 +1,16 @@
+const arr = [
+    'ishmael',
+    50,
+
+    function() {console.log("hey")},
+]
+
+arr[2]()
+
+for ( let i = 0; i < arr.length; i++) 
+{
+    console.log(arr[i]);
+
+}
+
+console.log(typeof arr);
