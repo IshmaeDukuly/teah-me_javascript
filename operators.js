e27acc1c94c96920705d5f0fb6cdb7050369b614
Moisema <moisemadukuly30@gmail.com>
2023-04-20 13:07:55 +0000

    .\operators.js

---
+++ operators.js
@@ -0,0 +1,34 @@
+
+
+"use strict";
+
+let num = 12;
+num = num+1; //addition
+console.log(num);
+
+let numeric = 60;
+numeric= numeric*2; //multiplication
+console.log(numeric);
+
+// division
+let myNum = 12;
+myNum = myNum/2;
+console.log(myNum);
+
+// Subtraction
+let digit = 5;
+digit = digit-2;
+console.log(digit);
+
+// Modulus or remainder
+let Num = 5;
+num = num%3;
+console.log(num);
+
+// Exponetial
+
+let MyNumber = 5;
+MyNumber = MyNumber**3;
+console.log(MyNumber);
+
+
