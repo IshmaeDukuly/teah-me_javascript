bcfca85c6159b7c159df90867ce68f308d74f308
Moisema <moisemadukuly30@gmail.com>
2023-04-24 06:16:44 +0000

    .\whle-loop.js

---
+++ whle-loop.js
@@ -0,0 +1,19 @@
+
+
+
+let i = 1;
+
+while (i < 100) {
+    console.log(i);
+    i++;
+}
+
+// Another loop starting here
+
+let n = 0;
+
+while (n <= 100) {
+    console.log(n);
+    n+=2;
+}
+    
